f1efd680126534f48b09aab649856f923c892aeb
neocentrica <64868274+neocentrica@users.noreply.github.com>
2022-12-10 11:31:11 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function openNav() {
+      document.getElementById("mySidepanel").style.height = "640px";
+    }
+    
+    /* Set the width of the sidebar to 0 (hide it) */
+    function closeNav() {
+      document.getElementById("mySidepanel").style.height = "0";
+    } 
